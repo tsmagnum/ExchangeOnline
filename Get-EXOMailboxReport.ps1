929eb9f8a0978fa1923b3bdf221eb5cd81701e21
Federico Lillacci <65031947+tsmagnum@users.noreply.github.com>
2021-09-02 20:27:10 +0200

    Add files via upload

--- Get-EXOMailboxReport.ps1
+++ Get-EXOMailboxReport.ps1
@@ -1,34 +1,3 @@
-<#
-.SYNOPSIS
-The scripts generates a detailed EXOL Mailbox report.
-.DESCRIPTION
-The scripts generates a detailed EXOL Mailbox report, with the information needed by EXOL admins. You can choose how you want to sort the data (default by Total Size), the report format (console, html or OutGrid-View) and whether to export the info in a CSV file.
-.PARAMETER sortBy
-Specify how to sort the report: choose between 'Name','Total Items','Last Logon Time'. Use single quotes to enclose the parameter.
-.PARAMETER reportFormat
-Select report format: console (default), html or ogv.
-.PARAMETER reportHtmlPath
-Name and (if desired) full filesystem path for the HTML report file; if omitted, it defaults to the script execution directory and a "EXOL_MBX_Report_<date>.html" filename.
-.PARAMETER reportCSVPath
-Name and (if desired) full filesystem path for the CSV report file; if omitted, it defaults to the script execution directory and a "EXOL_MBX_Report_<date>.csv" filename.
-.PARAMETER exportCSV
-Select if you want to export data in CSV format. You can specify where the report should be saved using -reportCSVPath parameter.
-.PARAMETER archive
-Select if you want to process archive mailboxes instead of regular mailboxes.
-.EXAMPLE
-Get-EXOMailboxReport.ps1 
-Running the script without any parameter will output a console report sorted by total size.
-.EXAMPLE
-Get-EXOMailboxReport.ps1 -sortBy "Last Logon Time"
-The script will output a console report sorted by last logon time.
-.EXAMPLE
-Get-EXOMailboxReport.ps1 -reportFormat html -reportHtmlPath "C:\mydir\myreport.html"
-The script will output a html report, named myreport.html in the c:\mydir directory.
-.EXAMPLE
-Get-EXOMailboxReport.ps1 -reportFormat ogv -exportCSV -reportCSVPath "C:\mydir\myreport.csv"
-The script will output a OutGrid-View report, exported in CSV and named myreport.csv in the c:\mydir directory.
-#>
-
 #region Credits
 # Author: Federico Lillacci - Coesione Srl - www.coesione.net
 # GitHub: https://github.com/tsmagnum
@@ -42,42 +11,48 @@ The script will output a OutGrid-View report, exported in CSV and named myreport
 #Requires -Modules @{ ModuleName="ExchangeOnlineManagement"; ModuleVersion="2.0.5" }
 
 [CmdletBinding()]
-param (
-        [Parameter(Mandatory=$false)]
-        [string]
-        $sortBy = "Total Size (GB)",
+$reportDate = Get-Date -UFormat %d%m%Y
 
-        [Parameter (Mandatory=$false)]
-        [string]
-        $reportFormat = "console",
+#######################################
+#region User-variables - General Settings
+$scheduled = $false #if $true, set the Unattended Connection variables
+$sortBy = "Total Size (GB)"
+$reportFormat = "console" # "console","html", "ogv"
+$exportCSV = $false
+$archive = $true
+$reportHtmlPath = "EXOL_MBX_Report_"+ $reportDate +".html"
+$reportCSVPath = "EXOL_MBX_Report_"+ $reportDate +".csv"
+#endregion
 
-        [Parameter(Mandatory=$false)]
-        [string]
-        $reportHtmlPath = "EXOL_MBX_Report_"+ (Get-Date -UFormat %d%m%Y) +".html",
+#region User-variables - Email Settings
+$sendEmail = $false
+$emailHost = "your.smtp.com"
+$emailPort = 25
+$emailEnableSSL = $true
+$emailUser = "yourUser"
+$emailPass = "yourPassword"
+$emailFrom = "exolReport@microsoft.com"
+$emailTo = "your@email.com"
+$emailSubject = "Exchange Online Mailbox Report"
+#endregion
 
-        [Parameter(Mandatory=$false)]
-        [string]
-        $reportCSVPath = "EXOL_MBX_Report_"+ (Get-Date -UFormat %d%m%Y) +".csv",
+#region User-variables - Unattended Connection (Scheduled Report)
+# Documentation: https://docs.microsoft.com/en-us/powershell/exchange/app-only-auth-powershell-v2?view=exchange-ps
+$certThumbPrint = "<insert here your certificate thumbprint>" #REQUIRED if you need to run the script as a scheduled task
+$appID = "<insert here your Azure app id>" #REQUIRED if you need to run the script as a scheduled task
+$org = "<domain>.onmicrosoft.com" #REQUIRED if you need to run the script as a scheduled task, use the default ".onmicrosoft.com" domain
 
-        [Parameter(Mandatory=$false)]
-        [switch]
-        $exportCSV,
+#DO NOT EDIT ANYTHING BEYOND THIS LINE!
+#######################################
+#endregion
 
-        [Parameter(Mandatory=$false)]
-        [switch]
-        $archive
-)
-
-#region Functions
-function reportHtml ($rawStats){
-
-#CSS Code
-        $header = @"
+#region CSS Code
+$header = @"
 <style>
     body
   {
       background-color: White;
-      font-size: 14px;
+      font-size: 12px;
       font-family: Arial, Helvetica, sans-serif;
   }
 
@@ -105,27 +80,55 @@ function reportHtml ($rawStats){
         border: 1px solid;
 }
 
-    h1{
+    h2{
         background-color: CornflowerBlue;
         color:white;
         text-align: center;
     }
 </style>
 "@
-#End CSS Code
+#endregion
 
-        $rawStats | `
-        ConvertTo-Html `
-                -PreContent "<h1>Exchange Online Mailbox Report</h1>"`
-                -PostContent "<p>Creation Date: $(Get-Date)<p>"`
-                -Title "EXOL Mailbox Report" `
-                -Head $header | `
-        Out-File -FilePath $reportHtmlPath
+#region HTML Code
+$preContent = "<h2>Exchange Online Mailbox Report</h2>"
+$postContent = "<p>Total Mailbox Count: $($stats.Count) - Creation Date: $(Get-Date)<p>"
+$title = "Exchange Online Mailbox Report"
+#endregion
+
+#region Functions
+function reportHtml {
+        
+        [CmdletBinding()]
+        Param(
+                [Parameter(Mandatory = $true)] $rawStats,
+                [Parameter(Mandatory = $false)] [switch] $generateFile
+        )
+        
+        if ($generateFile) 
+        { 
+                $rawStats | `
+                ConvertTo-Html `
+                        -PreContent $preContent `
+                        -PostContent $postContent `
+                        -Title $title `
+                        -Head $header | `
+                        Out-File -FilePath $reportHtmlPath
+        }
+
+        else 
+        {
+                $rawStats | `
+                ConvertTo-Html `
+                        -PreContent $preContent `
+                        -PostContent $postContent `
+                        -Title $title `
+                        -Head $header 
+        }
 }
 
 function reportConsole ($rawStats){
         $rawStats | Sort-Object -Property $sortBy -Descending | Format-Table -AutoSize -Wrap
-        Write-Host "Total Number of Mailboxes: " $mailboxes.count -ForegroundColor Green
+        Write-Host "Total Number of Mailboxes: " $rawStats.count -ForegroundColor Green
 }
 
 function reportOGV ($rawStats){
@@ -139,33 +142,46 @@ $ExolPSSession = Get-PSSession
 
 if ($ExolPSSession.ConfigurationName -ne "Microsoft.Exchange" -and $ExolPSSession.State -ne "Opened")
 {
-        #Connecting to Exchange Online
+        #Connecting to Exchange Online - if you want to run the script as a scheduled task, 
+        #please see https://office365itpros.com/2020/08/13/exo-certificate-based-authentication-powershell/
         Write-Host "We need to connect first to EXOL" -ForegroundColor Yellow
-        Connect-ExchangeOnline -ShowProgress $true
+        
+        if ($scheduled)
+        {
+                Connect-ExchangeOnline `
+                        -CertificateThumbprint $certThumbPrint `
+                        -AppId $appID `
+                        -ShowBanner:$false `
+                        -Organization $org
+        }
+        else 
+        {
+                Connect-ExchangeOnline -ShowProgress $true
+        }
 }
 
 # Getting mailbox data
-Write-Host "Processing your data, please wait..." -ForegroundColor Green
+Write-Host "Processing your mailboxes, please wait..." -ForegroundColor Green
+$mailboxes = @()
 
 if ($archive) 
 {
-        $mailboxes = Get-EXOMailbox -Archive | Get-EXOMailboxStatistics -PropertySets All
+        $mailboxes += Get-EXOMailbox -Archive -ResultSize Unlimited | Get-EXOMailboxStatistics -Archive -PropertySets All
 }
 
-else 
-{
-$mailboxes = Get-EXOMailbox | Get-EXOMailboxStatistics -PropertySets All
-}
+$mailboxes += Get-EXOMailbox -ResultSize Unlimited | Get-EXOMailboxStatistics -PropertySets All
 
+#Creating the raw stats 
 $stats = ( $mailboxes | Select-Object -property `
-@{Label = "Name"; Expression={$_.DisplayName}},`
-@{Label = "Total Items"; Expression={$_.ItemCount}},`
-@{Label = "Total Size (GB)"; Expression={[System.Math]::Round((($_.TotalItemSize.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}},`
-@{Label = "Deleted Items (GB)"; Expression={[System.Math]::Round((($_.TotalDeletedItemSize.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}},`
-@{Label = "Last Logon Time"; Expression={$_.LastLogonTime}},`
-@{Label = "Mailbox Type"; Expression={$_.MailboxTypeDetail}},`
-@{Label = "Storage Limit Status"; Expression={$_.StorageLimitStatus}}`
-| Sort-Object -Property $sortBy -Descending 
+                @{Label = "Name"; Expression={$_.DisplayName}},`
+                @{Label = "Total Size (GB)"; Expression={[System.Math]::Round((($_.TotalItemSize.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}},`
+                @{Label = "Total Items"; Expression={$_.ItemCount}},`               
+                @{Label = "Deleted Items (GB)"; Expression={[System.Math]::Round((($_.TotalDeletedItemSize.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}},`
+                @{Label = "Last Logon Time"; Expression={$_.LastLogonTime}},`
+                @{Label = "Mailbox Type"; Expression={$_.MailboxTypeDetail}},`
+                @{Label = "Archive Mbx"; Expression={$_.IsArchiveMailbox}},`
+                @{Label = "Storage Limit Status"; Expression={$_.StorageLimitStatus}}`
+|               Sort-Object -Property $sortBy -Descending 
 )
 
 # Choosing the desired report format
@@ -173,11 +189,24 @@ switch ($reportFormat)
 {
     console { reportConsole($stats)}
     ogv { reportOGV($stats) }
-    html { reportHtml($stats) }
+    html { reportHtml -rawStats $stats -generateFile }
 }
 
 # Exporting the data in CSV, if selected
 if ($exportCSV) 
 {
         $stats | Export-Csv -NoTypeInformation -Path $reportCSVPath
+}
+
+# Sending the report via email, if selected
+If ($sendEmail) 
+{
+        $smtp = New-Object System.Net.Mail.SmtpClient($emailHost, $emailPort)
+        $smtp.Credentials = New-Object System.Net.NetworkCredential($emailUser, $emailPass)
+        $smtp.EnableSsl = $emailEnableSSL
+        $msg = New-Object System.Net.Mail.MailMessage($emailFrom, $emailTo)
+        $msg.Subject = $emailSubject
+        $msg.Body = reportHtml -rawStats $stats
+        $msg.isBodyhtml = $true
+        $smtp.send($msg)
 }
